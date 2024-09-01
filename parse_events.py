0438caad808e911a5ebfdd4c3a2592cb58b4ac55
tlundy832 <tlundy@student.ubc.ca>
2024-09-01 15:07:46 -0700

    reorganized repo

--- parse_events.py
+++
@@ -1,9 +0,0 @@
-import json
-import ast
-f = open("events.json")
-lines = f.readlines()
-list_json = []
-for line in lines[0:10]:
-    line2 = ast.literal_eval(line)
-print(line2.keys())
-print(line2['collection_slug'])
